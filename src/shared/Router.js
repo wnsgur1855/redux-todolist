a2c761abdfd1541c66c51908f970d05a2b8882cb
junhyuck park <wnsgur1855@naver.com>
2023-02-15 01:53:27 +0900

    shared(라우터 path)

---
+++ src/shared/Router.js
@@ -0,0 +1,18 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Detail from '../pages/Detail';
+import Home from '../pages/Home';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<Detail />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
+
+//<Route path="detail/:id" element={<Detail />} />
