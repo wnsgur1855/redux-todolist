4d0f9d423a5ff39cb838ee710430c9683254e5e7
junhyuck park <wnsgur1855@naver.com>
2023-02-16 01:18:08 +0900

    폰트 전역으로 관리

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { createGlobalStyle } from 'styled-components';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
@@ -6,9 +7,19 @@ import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import store from './redux/config/configStore';
 
+const GlobalStyle = createGlobalStyle`
+@font-face {
+    font-family: 'KyoboHandwriting2021sjy';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_2212@1.0/KyoboHandwriting2021sjy.woff2') format('woff2');
+    font-weight: normal;
+    font-style: normal;
+}
+`;
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
+    <GlobalStyle />
     <App />
   </Provider>
 );
