bd29ccf53bd33c69a9bfd2e5f326f31985cf35f6
junhyuck park <wnsgur1855@naver.com>
2023-02-15 19:36:41 +0900

    라우터 추가

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -8,6 +8,7 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/:id" element={<Detail />} />
+        <Route path="/detail" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
